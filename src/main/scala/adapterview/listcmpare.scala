13cd280d0fa08b4968e251448f18784c849b141d
satanson <ranpanf@gmail.com>
2014-12-04 02:44:31 +0800

    [12/04/14 02:44:31] by satanson

--- src/main/scala/adapterview/listcmpare.scala
+++ src/main/scala/adapterview/listcmpare.scala
@@ -11,19 +11,35 @@ package adapterview
 import scala.Predef.{intWrapper=>_,_}
 
 object listcmpare {
-  implicit def list2ordered[T](L:List[T])(implicit t2ordered:T=>Ordered[T]): Ordered[List[T]] ={
+//  implicit def list2ordered[T](L:List[T])(implicit t2ordered:T=>Ordered[T]): Ordered[List[T]] ={
+//    class ListOrdered(lhs:List[T]) extends Ordered[List[T]]{
+//      def compare(rhs:List[T]):Int={
+//        if (lhs.isEmpty && !rhs.isEmpty)return 1
+//        else if(lhs.isEmpty && rhs.isEmpty)return 0
+//        else if(!lhs.isEmpty && rhs.isEmpty)return -1
+//        else {
+//          if (lhs.head!=rhs.head) {
+//            return t2ordered(lhs.head).compare(rhs.head)
+//          }else
+//            return new ListOrdered(lhs.tail).compare(rhs.tail)
+//        }
+//      }
+//    }
+//    new ListOrdered(L)
+//  }
+  implicit def list2orderedA[T](L:List[T])(implicit t2ordered:T=>Ordered[T]): Ordered[List[T]] ={
     class ListOrdered(lhs:List[T]) extends Ordered[List[T]]{
       def compare(rhs:List[T]):Int={
-        if (lhs.isEmpty && !rhs.isEmpty)return 1
-        else if(lhs.isEmpty && rhs.isEmpty)return 0
-        else if(!lhs.isEmpty && rhs.isEmpty)return -1
+        if (lhs.isEmpty && !rhs.isEmpty) 1
+        else if(lhs.isEmpty && rhs.isEmpty) 0
+        else if(!lhs.isEmpty && rhs.isEmpty) -1
         else {
           if (lhs.head!=rhs.head) {
-            return t2ordered(lhs.head).compare(rhs.head)
+            lhs.head compare rhs.head
           }else
-            return new ListOrdered(lhs.tail).compare(rhs.tail)
+            lhs.tail compare rhs.tail
         }
-      } 
+      }
     }
     new ListOrdered(L)
   }
