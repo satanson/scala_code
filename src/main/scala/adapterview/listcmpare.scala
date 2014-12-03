314f7fd19a9eb0ef0aacc876b8834047cf3d04c8
satanson <ranpanf@gmail.com>
2014-12-04 02:24:51 +0800

    [12/04/14 02:24:51] by satanson

---
+++ src/main/scala/adapterview/listcmpare.scala
@@ -0,0 +1,43 @@
+package adapterview
+
+/**
+ * Created by satanson on 2014/12/4.
+ */
+
+//import scala.Predef.{_,intWrapper=>_} //WildCard import must be in last position
+//intWrapper in scala.Predef(in fact,in LowPriorityImplicits) causes ambiguity
+//because there are two implicit function :Int=>Ordered[Int]
+//so not import intWrapper
+import scala.Predef.{intWrapper=>_,_}
+
+object listcmpare {
+  implicit def list2ordered[T](L:List[T])(implicit t2ordered:T=>Ordered[T]): Ordered[List[T]] ={
+    class ListOrdered(lhs:List[T]) extends Ordered[List[T]]{
+      def compare(rhs:List[T]):Int={
+        if (lhs.isEmpty && !rhs.isEmpty)return 1
+        else if(lhs.isEmpty && rhs.isEmpty)return 0
+        else if(!lhs.isEmpty && rhs.isEmpty)return -1
+        else {
+          if (lhs.head!=rhs.head) {
+            return t2ordered(lhs.head).compare(rhs.head)
+          }else
+            return new ListOrdered(lhs.tail).compare(rhs.tail)
+        }
+      } 
+    }
+    new ListOrdered(L)
+  }
+  implicit def Int2Ordered(i:Int):Ordered[Int]={
+    class IntOrdered(lhs:Int) extends Ordered[Int]{
+      def compare(rhs:Int):Int=rhs-lhs
+    }
+    new IntOrdered(i)
+  }
+  def main(args:Array[String]):Unit={
+    val L1=List(1,2,3)
+    val L2=List(1,2,2)
+    println(L1<L2)
+    println(L1>L2)
+    println(L1==L1)
+  }
+}
